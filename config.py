8947119b4398eeef6f47fc91dc64cc2908dd12cf
BookWorm <infohubnetworks@gmail.com>
2024-12-16 23:56:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "21145186"))
 API_HASH = environ.get("API_HASH", "daa53f4216112ad22b8a8f6299936a46")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7681830527:AAHIy7NeWz06sK6EeBLPxogBCLR1AGFvX0c")
-ADMIN = int(environ.get("ADMIN", "6011680723 5178714818"))          
+ADMIN = int(environ.get("ADMIN", "6011680723"))          
 CAPTION = environ.get("CAPTION", "@Bookslibraryofficial")
 
 # for thumbnail ( back end is MrMKN brain 😉)
