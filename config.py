481f0b774967d211dce07b4974c36c99d59a7261
BookWorm <infohubnetworks@gmail.com>
2024-12-16 23:53:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "21145186"))
+API_HASH = environ.get("API_HASH", "daa53f4216112ad22b8a8f6299936a46")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7681830527:AAHIy7NeWz06sK6EeBLPxogBCLR1AGFvX0c")
+ADMIN = int(environ.get("ADMIN", "6011680723 5178714818"))          
+CAPTION = environ.get("CAPTION", "@Bookslibraryofficial")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
